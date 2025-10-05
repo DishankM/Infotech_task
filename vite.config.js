39f4bb342f437b1be290a86ad027f7d26a038166
DishankM <dishankmahajan424@gmail.com>
2025-10-05 14:13:19 +0530

    commit third

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import Checker, { checker } from 'vite-plugin-checker';
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
      tailwindcss(),
-     tsconfigPaths(),
-     Checker({ typescript: true })
+     tsconfigPaths()
   ],
 
 })
